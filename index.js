1a722916e5b630c267e293aadd0ee0a91cbca2b8
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-12-28 13:43:40 +0530

    debouncing applied

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.de
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 const movieBox = document.querySelector(".container")
+let debounceTimeout;
 const getMovieDetails = async(api)=>{
 
   const response = await fetch(api);
@@ -11,11 +12,12 @@ const getMovieDetails = async(api)=>{
    showMovie(data.results)
 
 }
-
+function sortByRating(){
+  
+}
 const showMovie =(data)=>{
   movieBox.innerHTML=""
-  
-//  console.log("Dataaa",data.results)
+
  data.map((item,i)=>{
   console.log("item : ",IMGPATH+item.poster_path)
    const box = document.createElement("div");
@@ -31,15 +33,20 @@ const showMovie =(data)=>{
  })
 }
 
-document.getElementById("search").addEventListener("keyup", function(event){
-  console.log("event ",event.target.value)
-  if(event.target.value !=""){
-    getMovieDetails(SEARCHAPI+event.target.value)
+const handleSearch = (event) => {
+  console.log("event ", event.target.value);
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(() => {
+    if (event.target.value !== "") {
+      getMovieDetails(SEARCHAPI + event.target.value);
+    } else {
+      getMovieDetails(APIURL);
+    }
+  }, 300); 
+};
 
-  }else{
-    getMovieDetails(APIURL)
-  }
-})
-getMovieDetails(APIURL)
+document.getElementById("search").addEventListener("keyup", handleSearch);
+getMovieDetails(APIURL);
+
 
 
